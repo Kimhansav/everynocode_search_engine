4ae9fbb4ef1daef3cf763c5fcbbf6480d29ec39e
Kimhansav <134425555+Kimhansav@users.noreply.github.com>
2024-05-21 22:20:41 +0900

    Add files via upload

--- server/app.py
+++ server/app.py
@@ -7,8 +7,8 @@ import time
 import json
 import ast
 import torch
-import logging
-from flask import Flask, request, jsonify
+from functools import wraps
+from flask import Flask, request, jsonify, make_response, Response
 from transformers import AutoModel, AutoTokenizer
 from google.cloud import storage
 from datetime import datetime, timedelta, timezone
@@ -18,22 +18,9 @@ tokenizer = None
 index = None
 data = None
 
-#로컬 테스트를 위한 signed_url 생성기
-def generate_signed_url(bucket_name, blob_name):
-    """Generate a signed URL for the given bucket and blob that expires in one hour."""
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(blob_name)
-
-    url = blob.generate_signed_url(
-        expiration=datetime.now(timezone.utc) + timedelta(hours=1),
-        method='GET' 
-    )
-    return url
-
 def create_app():
     app = Flask(__name__)
-    logging.basicConfig(level = logging.DEBUG)
+    # app.config['JSON_AS_ASCII'] = False
 
     with app.app_context():
         init_db()
@@ -44,22 +31,29 @@ def create_app():
         # 유사도 계산 및 가장 유사한 텍스트 찾기 (여기서는 로직 구현 필요)
         results = search(user_input)
         return jsonify(results)
-
     return app
 
 #모델 경로를 지정
 HUGGINGFACE_MODEL_PATH = 'BM-K/KoSimCSE-bert-multitask'
-signed_url = generate_signed_url('bubble_project', 'sentence_embed_result_short.csv')
 
-# #Google cloud storage에서 파일을 다운로드하는 함수
-# def download_blob(bucket_name, source_blob_name, destination_file_name):
-#     """GCS에서 파일을 다운로드하고 로컬 파일 시스템에 저장합니다."""
-#     storage_client = storage.Client()
-#     bucket = storage_client.bucket(bucket_name)
-#     blob = bucket.blob(source_blob_name)
-#     blob.download_to_filename(destination_file_name)
+# #json 데이터를 리턴할 때 한글이 깨지는 현상을 막기 위한 데코레이터
+# def as_json(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         res = f(*args, **kwargs)
+#         res = json.dumps(res, ensure_ascii = False).encode('utf8')
+#         return Response(res, content_type = 'application/json; charset = utf-8')
+#     return decorated_function
 
-#     print(f"{source_blob_name} downloaded to {destination_file_name}.")
+#Google cloud storage에서 파일을 다운로드하는 함수
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """GCS에서 파일을 다운로드하고 로컬 파일 시스템에 저장합니다."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+    print(f"{source_blob_name} downloaded to {destination_file_name}.")
 
 #검색 함수에서 사용할 사용자 입력 인코딩 함수
 def encode(query):
@@ -81,11 +75,29 @@ def search(query):
   try:
     t = time.time()
     query_vector = encode(query)
-    k = 20
+    k = 5
     top_k = index.search(query_vector, k)
     print('total time: {}'.format(time.time() - t))
+    top_k_ids = top_k[1].tolist()[0]
+    #bubble.io의 api connector가 데이터를 json으로 읽을 수 있도록 형식 변경
+    # results = {}
+    # top_k_ids = top_k[1].tolist()[0]
+    # for i, _id in enumerate(top_k_ids):
+    #     #url이 비어 있으면(카톡 대화문이면) check = True
+    #     check = True if pd.isna(data.iloc[_id]['url']) == True else False
+    #     results[f'{i} question'] = data.iloc[_id]['question']
+    #     results[f'{i} answer'] = ' '.join(s for s in ast.literal_eval(data.iloc[_id]['answer'])) if check == True else data.iloc[_id]['answer']
+    #     results[f'{i} question summary'] = data.iloc[_id]['question_summary']
+    #     results[f'{i} answer summary'] = data.iloc[_id]['answer_summary']
+    #     results[f'{i} question with url'] = data.iloc[_id]['question_withurl']
+    #     results[f'{i} answer with url'] = ' '.join(s for s in ast.literal_eval(data.iloc[_id]['answer_withurl'])) if check == True else data.iloc[_id]['answer_withurl']
+    #     results[f'{i} questioner'] = data.iloc[_id]['questioner']
+    #     results[f'{i} respondent'] = ' '.join(s for s in ast.literal_eval(data.iloc[_id]['respondent'])) if check == True else '.'
+    #     results[f'{i} date'] = data.iloc[_id]['date']
+    #     results[f'{i} url'] = '.' if check == True else data.iloc[_id]['url']
 
-    return [{'question' : data.iloc[_id]['question'], 'answer' : data.iloc[_id]['answer'], 'question_summary' : data.iloc[_id]['question_summary'], 'answer_summary' : data.iloc[_id]['answer_summary'], 'question_withurl' : data.iloc[_id]['question_withurl'], 'answer_withurl' : data.iloc[_id]['answer_withurl'], 'questioner' : data.iloc[_id]['questioner'], 'respondent' : data.iloc[_id]['respondent'], 'date' : data.iloc[_id]['date'], 'url' : data.iloc[_id]['url']} for _id in top_k[1].tolist()[0]]
+    # return results
+    return {i : {'question' : data.iloc[_id]['question'], 'answer' : data.iloc[_id]['answer'], 'question_summary' : data.iloc[_id]['question_summary'], 'answer_summary' : data.iloc[_id]['answer_summary'], 'question_withurl' : data.iloc[_id]['question_withurl'], 'answer_withurl' : data.iloc[_id]['answer_withurl'], 'questioner' : data.iloc[_id]['questioner'], 'respondent' : data.iloc[_id]['respondent'], 'date' : data.iloc[_id]['date'], 'url' : data.iloc[_id]['url']} for i, _id in enumerate(top_k_ids)}
   except Exception as e:
       app.logger.error(f'Error in search: {str(e)}')
       raise e
@@ -98,8 +110,8 @@ def load_model_tokenizer():
 #초기 호출 시 csv 데이터를 데이터프레임으로 변형, 데이터프레임과 FAISS 인덱스를 전역 변수로 서버에 캐싱하는 함수
 def load_df_index():
     #데이터 불러오기
-    # download_blob('bubble_project', 'data/sentence_embed_result.csv', '/tmp/sentence_embed_result.csv')
-    df = pd.read_csv(signed_url)
+    download_blob('bubble_project', 'sentence_embed_result_short.csv', '/tmp/sentence_embed_result_short.csv')
+    df = pd.read_csv('/tmp/sentence_embed_result_short.csv')
     # df = pd.read_csv('sentence_embed_result_short.csv')
     #csv파일로 저장할 때 쉼표를 살리기 위해 json.dumps로 저장했기 때문에 json.loads 사용
     df['embedding'] = df['embedding'].apply(json.loads)
@@ -121,13 +133,6 @@ def init_db():
     if model is None or tokenizer is None or index is None or data is None:
         model, tokenizer = load_model_tokenizer() # 모델, 토크나이저 로딩 함수
         data, index = load_df_index() #데이터프레임, 인덱스 로딩 함수
-
-# @app.route('/search', methods=['GET'])
-# def find_similar():
-#     user_input = request.args.get('q')
-#     # 유사도 계산 및 가장 유사한 텍스트 찾기 (여기서는 로직 구현 필요)
-#     results = search(user_input)
-#     return jsonify(results)
 
 if __name__ == '__main__':
     app = create_app()
